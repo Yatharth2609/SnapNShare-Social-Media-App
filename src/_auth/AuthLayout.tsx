f2456690519d2e44585c1915e0f181ad04cead82
Yatharth Mishra <91468250+Yatharth2609@users.noreply.github.com>
2023-12-28 23:21:36 +0530

    Update AuthLayout.tsx side-img

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
           </section>
 
           <img
-            src="\assets\images\side-img.svg"
+            src="\assets\images\ecb1af79-7908-4310-960d-b48227a7ca84.jpg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
